3fa2dc0f2be88fea5d1caf8f5e3391cfee0b3815
Nadeem Sajjad <nady5730642@gmail.com>
2021-08-25 17:18:37 +0530

    minor (#1)
    Co-authored-by: nadeemcite <nadeem.sajjad.1991@gmail.com>

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import constants from './constants';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -15,8 +16,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it(`should return "${constants.WELCOME_MSG}"`, () => {
+      expect(appController.getHello()).toBe(constants.WELCOME_MSG);
     });
   });
 });
--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import Constants from './constants';
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return Constants.WELCOME_MSG;
   }
 }
